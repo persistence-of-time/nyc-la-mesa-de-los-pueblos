1b6c8d59d9a64522635d22c432c8fe9db206db5c
persistence-of-time <gustavo.garcia14@journalism.cuny.edu>
2022-12-26 11:51:29 -0500

    updates limited internet

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ const watch = new Watch(video_frame, {
   threshold: 0.75,
 });
 let player;
+
+let observer = new IntersectionObserver((entries, observer)=> {
+  entries.forEach((entry) => {
+    if (window.matchMedia("(preferes-reduced-motion: reduce)").matches) {
+
+      vid_trig.currentTime = 2;
+    } else {
+      if (entry.isIntersecting) {
+        vid_trig.play();
+      }
+    }
+  });
+});
+
+observer.observe(vid_trig);
