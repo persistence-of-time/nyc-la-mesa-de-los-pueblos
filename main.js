abda1ac00eacbb106093903af9c8a738e88c4276
persistence-of-time <gustavo.garcia14@journalism.cuny.edu>
2023-01-24 03:18:54 -0500

    updates to js

--- main.js
+++ main.js
@@ -4,4 +4,40 @@ const pg1 = document.querySelector('.pg1')
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
   pg1.classList.toggle('active')
-})
+});
+
+// slideshow cotrols 
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const slider = document.querySelector(".slider");
+const img = document.querySelectorAll(".slider img");
+
+let index = 0;
+let width = img[index].clientWidth;
+
+next.addEventListener("click", () => {
+  index++;
+
+  slider.style.transform = `translate(${-index * (width + 10)}px)`;
+
+  if (index === img.length -1) {
+    next.classList.add("disable");
+  } else {
+    prev.classList.remove("disable")
+  }
+});
+
+prev.addEventListener("click", () => {
+  index--;
+
+  slider.style.transform = `translate(${-index * (width + 10)}px)`;
+
+  if (index === 0) {
+    prev.classList.add("disable");
+  } else {
+    next.classList.remove("disable");
+  }
+});
+
+prev.classList.add("disable")
